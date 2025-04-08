455dde628638bb6d8e6251430cd02a1eb0064d2f
Pronoy-Deb <pronoydebnath2017@gmail.com>
2025-04-08 18:29:28 +0600

    Done the final customization

---
+++ nopCommerce_4.80.5_Source/src/Presentation/Nop.Web/Components/HomepageBrochureViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Web.Components;
+public class HomepageBrochureViewComponent : NopViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View();
+    }
+}
