45054098c9277927c87e62fa383bf5bd7ee8325b
Pronoy-Deb <pronoydebnath2017@gmail.com>
2025-04-18 11:29:00 +0600

    Fix the issue of uninstallation

--- nopCommerce_4.80.5_Source/src/Plugins/Nop.Plugin.Misc.Suppliers/Data/Migrations/SchemaMigration.cs
+++ nopCommerce_4.80.5_Source/src/Plugins/Nop.Plugin.Misc.Suppliers/Data/Migrations/SchemaMigration.cs
@@ -7,7 +7,7 @@ using Nop.Data.Extensions;
 using Nop.Data.Migrations;
 
 [NopSchemaMigration("2025/04/11 06:50:00", "Suppliers base schema", MigrationProcessType.Installation)]
-public class SchemaMigration : ForwardOnlyMigration
+public class SchemaMigration : AutoReversingMigration
 {
     public override void Up()
     {
